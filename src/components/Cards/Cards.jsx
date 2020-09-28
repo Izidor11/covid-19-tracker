502332291cef13a78aca882e33671a1bf3e0a567
tycoon <smizidor@gmail.com>
2020-09-28 14:00:39 +0200

    first card

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -6,7 +6,18 @@ import styles from './Cards.module.css';
 const Cards = (props) => {
     console.log(props);
     return (
-        <div className>={styles.container}</div>
+        <div className={styles.container}>
+        <Grid container spacing={3} justify="center">
+        <Grid item component = {Card}>
+            <CardContent>
+                <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                <Typography variant="h5" gutterBottom>REAL DATA</Typography>
+                <Typography color="textSecondary" >REAL time</Typography>
+                <Typography variant="body2" >Number of active cases of Covid-19</Typography>
+            </CardContent>
+        </Grid>
+        </Grid>
+        </div>
     )
 }
 
